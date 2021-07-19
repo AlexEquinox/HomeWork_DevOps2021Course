2f8fcaab119e12faf6da5b875e57dfa2184a9055
Alex Equinox <abahirev@gmail.com>
2021-07-19 17:04:03 +0300

    homework_3.5

---
+++ hw3_5/scripts/root_or_not/test_root.sh
@@ -0,0 +1,11 @@
+#!usr/bin/bash
+
+ROOT_UID=0
+
+if [ "$UID" -eq "$ROOT_UID" ]
+then
+echo "You are root."
+else
+echo "You are not root"
+fi
+
---
+++ hw3_5/scripts/show_last_upd_file/last.sh
@@ -0,0 +1,3 @@
+#!usr/bin/bash
+
+ls -lrt | grep ^- | awk 'END{print $NF}'
